ed6558a8266f7520cfd179c01b449989961ee6d0
Goyal <tanu.b.goyal@accenture.com>
2020-06-12 00:22:50 +0530

    twelve  commit

--- src/containers/NewProfilePortal/NewProfilePortal.js
+++ src/containers/NewProfilePortal/NewProfilePortal.js
@@ -7,7 +7,6 @@ const NewProfilePortalModal= (props) =>{
    return ReactDom.createPortal(
       <div
         style={{
-          position: 'absolute',
           top: '12rem',
           bottom: '0',
           left: '0',
@@ -18,15 +17,17 @@ const NewProfilePortalModal= (props) =>{
           backgroundColor: '#a4508b',
           backgroundImage: 'linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)',
           height: '250px',
+          marginTop:'4rem'
         }}
         onClick={props.onClose}
       >
         <div
           style={{
+            zIndex:'2',
             padding: 20,
             background: '#fff',
             borderRadius: '1rem',
-            margin: '1rem',
+            margin: '5rem',
             position: 'relative',
             minWidth: '400px',
             boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
