657123e2c50f675aa2834e94525da58fcdd31d26
Goyal <tanu.b.goyal@accenture.com>
2020-06-14 18:00:39 +0530

    nav fixed

--- src/containers/ExtensionBody/ExtensionBody.js
+++ src/containers/ExtensionBody/ExtensionBody.js
@@ -14,11 +14,11 @@ import { AiOutlineCloseCircle } from "react-icons/ai";
 // import ExtensionBody from "../src/containers/ExtensionBody/ExtensionBody";
 // import NewProfileBody from "../NewProfileBody/NewProfileBody";
 export default class ExtensionBody extends Component {
-  openNewTab(event, route) {
-    // console.log(event, route)
-    event.preventDefault();
-    window.open(route);
-  }
+  // openNewTab(event, route) {
+  //   // console.log(event, route)
+  //   event.preventDefault();
+  //   window.open(route);
+  // }
   render() {
     return (
       <>
@@ -26,42 +26,42 @@ export default class ExtensionBody extends Component {
           <NavLink
             className="button"
             to="/profile"
-            onClick={(event) => this.openNewTab(event, "/profile")}
+            // onClick={(event) => this.openNewTab(event, "/profile")}
           >
             <BsThreeDotsVertical />
           </NavLink>
           <NavLink
             className="button"
             to="/workplace-links"
-            onClick={(event) => this.openNewTab(event, "/workplace-links")}
+            // onClick={(event) => this.openNewTab(event, "/workplace-links")}
           >
             <MdWork />
           </NavLink>
           <NavLink
             className="button"
             to="/community-links"
-            onClick={(event) => this.openNewTab(event, "/community-links")}
+            // onClick={(event) => this.openNewTab(event, "/community-links")}
           >
             <FaHandsHelping />
           </NavLink>
           <NavLink
             className="button"
             to="/learning-links"
-            onClick={(event) => this.openNewTab(event, "/learning-links")}
+            // onClick={(event) => this.openNewTab(event, "/learning-links")}
           >
             <MdSchool />
           </NavLink>
           <NavLink
             className="button"
             to="/personal-links"
-            onClick={(event) => this.openNewTab(event, "/personal-links")}
+            // onClick={(event) => this.openNewTab(event, "/personal-links")}
           >
             <GiSelfLove />
           </NavLink>
           <NavLink
             className="button"
             to="/add-new-profile"
-            onClick={(event) => this.openNewTab(event, "/add-new-profile")}
+            // onClick={(event) => this.openNewTab(event, "/add-new-profile")}
           >
             <BsPersonPlusFill />
           </NavLink>
